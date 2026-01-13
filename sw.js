5a7a515ea95538f3a9f45e0255d592a3cd921d7b
Arun Bamal <arunbamal15@gmail.com>
2026-01-13 21:31:14 +0530

    Update PWA manifest and service worker for offline and screenshots

--- sw.js
+++ sw.js
@@ -1,1 +1,40 @@
-self.addEventListener("fetch", () => {});
+const CACHE_NAME = "arrows-escape-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/icon-192.png",
+  "/icon-512.png",
+  "/manifest.json",
+  "/screenshot1.png",
+  "/screenshot2.png"
+];
+
+// Install service worker and cache files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate service worker and remove old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Intercept fetch requests and serve cached files
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
